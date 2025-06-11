3141d9dd48340a0d54010bffae7220f4763716d0
Reinout Dam <reinoutdam@gmail.com>
2025-06-11 12:57:41 +0200

    add config example file

---
+++ config_example.py
@@ -0,0 +1,11 @@
+#base url Pure instance
+PURE_BASE_URL = 'https://research.accept.vu.nl'
+
+#key to old - final version - Pure API (read only)
+PURE_524_API_KEY = '***'
+
+#key to new CRUD Pure API
+PURE_CRUD_API_KEY = '***'
+
+#scopus key for abstracts API
+SCOPUS_API_KEY = '***'
